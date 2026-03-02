e4fa68791f2eadb5a506457f3261db5f1276dec1
vamsi272325 <rr09368053357@gmail.com>
2026-03-02 16:17:27 +0530

    uc8- welcome

--- app/src/Palindromecheckerapp.java
+++ app/src/Palindromecheckerapp.java
@@ -1,26 +1,80 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class Palindromecheckerapp {
 
-    public static void main(String[] args) {
+    static class Node {
+        char data;
+        Node next;
 
-        String input = "racecar";
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
+        Node(char data) {
+            this.data = data;
+            this.next = null;
         }
+    }
+
+    static Node buildList(String input) {
+        Node head = null;
+        Node tail = null;
+        for (char c : input.toCharArray()) {
+            Node newNode = new Node(c);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+
+    static Node findMiddle(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    static Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    static boolean checkPalindrome(String input) {
+        Node head = buildList(input);
+
+        Node middle = findMiddle(head);
+        Node secondHalf = reverseList(middle);
+
+        Node first = head;
+        Node second = secondHalf;
 
         boolean isPalindrome = true;
 
-        while (deque.size() > 1) {
-            if (deque.pollFirst() != deque.pollLast()) {
+        while (second != null) {
+            if (first.data != second.data) {
                 isPalindrome = false;
                 break;
             }
+            first = first.next;
+            second = second.next;
         }
+
+        return isPalindrome;
+    }
+
+    public static void main(String[] args) {
+        String input = "madam";
+
+        boolean isPalindrome = checkPalindrome(input);
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
