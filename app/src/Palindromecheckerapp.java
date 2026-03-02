fedeac48660f829706bd9b83f1b0cc61b39b3cc1
vamsi272325 <rr09368053357@gmail.com>
2026-03-02 16:20:27 +0530

    uc9- welcome

--- app/src/Palindromecheckerapp.java
+++ app/src/Palindromecheckerapp.java
@@ -1,82 +1,21 @@
 public class Palindromecheckerapp {
 
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    static Node buildList(String input) {
-        Node head = null;
-        Node tail = null;
-        for (char c : input.toCharArray()) {
-            Node newNode = new Node(c);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        return head;
-    }
-
-    static Node findMiddle(Node head) {
-        Node slow = head;
-        Node fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    static Node reverseList(Node head) {
-        Node prev = null;
-        Node curr = head;
-        while (curr != null) {
-            Node next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-
-    static boolean checkPalindrome(String input) {
-        Node head = buildList(input);
-
-        Node middle = findMiddle(head);
-        Node secondHalf = reverseList(middle);
-
-        Node first = head;
-        Node second = secondHalf;
-
-        boolean isPalindrome = true;
-
-        while (second != null) {
-            if (first.data != second.data) {
-                isPalindrome = false;
-                break;
-            }
-            first = first.next;
-            second = second.next;
-        }
-
-        return isPalindrome;
-    }
-
     public static void main(String[] args) {
         String input = "madam";
 
-        boolean isPalindrome = checkPalindrome(input);
+        boolean isPalindrome = check(input, 0, input.length() - 1);
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+        return check(s, start + 1, end - 1);
     }
 }
